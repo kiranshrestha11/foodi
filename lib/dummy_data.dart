431f3e7bd99d898bafa3abebd9f588637b1ec38f
kiranshrestha11 <kiranstha1679@gmail.com>
2020-02-26 17:34:30 +0545

    created new app

---
+++ lib/dummy_data.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'models/category.dart';
+
+const List<Category> Dummy_CATEGORIES = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Fast foods',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Thakali',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Mexican',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Chinese',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Nepali',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Indian',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Vegan',
+    color: Colors.grey,
+  ),
+];
---
+++ lib/screens/category_overview_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:foodi/widgets/category_item.dart';
+import 'package:foodi/dummy_data.dart';
+
+class CaregoryOverviewScreeen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to Foodi App'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(15),
+        children: Dummy_CATEGORIES.map((catData) {
+          return CategoryItem(catData.title, catData.color);
+        }).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200.0,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
